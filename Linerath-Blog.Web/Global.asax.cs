56d0426bf2a47679b175d50607440a6089b756f3
Linerath <linerath@yandex.ru>
2019-01-01 17:39:57 +0300

    Added Ninject

--- Linerath-Blog.Web/Global.asax.cs
+++ Linerath-Blog.Web/Global.asax.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Linerath_Blog.Web.Infrastructure;
+using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Mvc;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Linerath_Blog.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            NinjectModule ninjectModule = new NinjectDependencies();
+            var kernel = new StandardKernel(ninjectModule);
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }
     }
 }
---
+++ Linerath-Blog.Web/Infrastructure/NinjectDependencies.cs
@@ -0,0 +1,11 @@
+﻿using Ninject.Modules;
+
+namespace Linerath_Blog.Web.Infrastructure
+{
+    public class NinjectDependencies : NinjectModule
+    {
+        public override void Load()
+        {
+        }
+    }
+}
