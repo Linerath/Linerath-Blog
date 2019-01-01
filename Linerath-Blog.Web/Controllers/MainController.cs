6ce0871ea6b5fe3af5dccf2372f8c2cb5a5cc838
Linerath <linerath@yandex.ru>
2019-01-01 17:55:30 +0300

    Added controller, view and updated packages

---
+++ Linerath-Blog.Web/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Linerath_Blog.Web.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
