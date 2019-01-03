ab05bdac2e426ab1632efad1ddacb589d5fb4f1f
Alexey Galyuk <linerath@mail.ru>
2019-01-03 13:23:02 +0300

    Added Tests project for unit testing

---
+++ Linerath-Blog.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Linerath-Blog.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linerath-Blog.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02bfd336-ace6-4470-bedf-2749582fb294")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Linerath-Blog.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Linerath_Blog.DAL.Repositories;
+using Linerath_Blog.Web.Controllers;
+using Linerath_Blog.Web.Infrastructure.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Linerath_Blog.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetControllerNameWorks()
+        {
+            //Mock<UnitOfWork> mock = new Mock<UnitOfWork>();
+            //MockArticleRepository articleRepository = new MockArticleRepository();
+            //mock.Setup(x => x.ArticleRepository).Returns(articleRepository);
+            //ArticlesController articlesController = new ArticlesController(mock.Object);
+
+            //String name = articlesController.GetControllerName();
+
+            //Assert.IsTrue(name == "Articles");
+        }
+    }
+}
