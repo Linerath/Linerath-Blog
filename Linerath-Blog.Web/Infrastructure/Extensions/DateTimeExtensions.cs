3bdab1065fc9aaf7a6ada1f8bfc8f7d20b615a30
Linerath <linerath@mail.ru>
2019-02-14 16:23:15 +0300

    Fixed DateTime format via extension

---
+++ Linerath-Blog.Web/Infrastructure/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Linerath_Blog.Web.Infrastructure.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static String ToUIString(this DateTime dateTime)
+        {
+            CultureInfo cultureInfo = new CultureInfo("ru-RU");
+
+            return dateTime.ToString("dddd, dd MMMM yyyy HH:ss", cultureInfo);
+        }
+    }
+}
